52b6c2351c572e3a04bcc75351e4b6dfa2b3db7a
toshi443 <toshi.443.aki@gmail.com>
2016-04-30 17:41:11 +0000

    aaa

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,40 @@
+class PostsController < ApplicationController
+    def index
+      @posts = Post.all
+    end
+    
+    def show
+      @post = Post.find(params[:id])
+    end
+    
+    def new
+      @post = Post.new
+    end
+    
+    def create
+      post = Post.new(post_params)
+      post.save
+      redirect_to root_path
+    end
+    
+    def update
+      post = Post.find(params[:id])
+      post.update_attributes(post_params)
+      redirect_to root_path
+    end
+    
+    def edit
+      @post = Post.find(params[:id])
+    end
+    
+    def destroy
+      post = Post.find(params[:id])
+      post.destroy
+      redirect_to root_path
+    end
+    
+    private
+      def post_params
+        params.require(:post).permit(:title, :content)
+      end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,26 @@
 Rails.application.routes.draw do
 
+
+
+  get 'sessions/new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'static_pages#top'
+  
+  resources :users
+  resources :posts
+  
+  get 'login' => 'sessions#new'
+  
+  post 'login' => 'sessions#create'
+  
+  get 'logout' => 'sessions#destroy'
+  
+  
+  
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
